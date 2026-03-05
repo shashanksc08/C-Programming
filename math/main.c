b03ace1deda7d6723866d08c56cb51a87febb116
shashanksc08 <shashanksc210@gmail.com>
2026-03-05 12:38:30 +0530

    added math folder (Practiced)

---
+++ math/main.c
@@ -0,0 +1,13 @@
+#include "math.h"
+#include <stdio.h>
+
+
+
+int main(void){
+	printf("Add : %d \n",math_add(123424,123424));
+	printf("Multiply : %I64d \n",math_mul(1234998,123424));
+ 	printf("Divide : %f \n",math_div(100,8));
+//	printf("Add : %d \n",math_add(0x0EEF1234,0x0EEF1234));
+
+	return 0;
+}
---
+++ math/math.c
@@ -0,0 +1,26 @@
+
+int math_add(int n1,int n2);
+int math_sub(int n1,int n2);
+long long math_mul(int n1,int n2);
+float math_div(int n1,int n2);
+
+
+int math_add(int n1,int n2){
+
+	return n1+n2;
+}
+
+int math_sub(int n1,int n2){
+
+	return n1-n2;
+}
+
+long long math_mul(int n1,int n2){
+
+	return (long long)n1*n2;
+}
+
+float math_div(int n1,int n2){
+
+	return (float)n1/n2;
+}
